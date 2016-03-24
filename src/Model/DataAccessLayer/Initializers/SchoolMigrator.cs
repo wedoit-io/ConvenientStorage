f53941750512b1032d7b04a20bbb147a8180328b
Ali Servet Donmez <asd@pittle.org>
2016-03-24 10:44:55 +0100

    Remove unnecessary files

--- src/Model/DataAccessLayer/Initializers/SchoolMigrator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Model.DataAccessLayer.Initializers
-{
-    using Model.DataAccessLayer.Contexts;
-    using Model.Migrations;
-
-    public class SchoolMigrator : System.Data.Entity.MigrateDatabaseToLatestVersion<SchoolContext, Configuration>
-    {
-    }
-}
